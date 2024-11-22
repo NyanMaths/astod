b8d57a7bb1b0d4ff8b71cb16aa1f8c85f438ff83
Apo35310 <apollinairebsy@gmail.com>
2024-11-22 08:59:52 +0100

    Create LivingEntity and Map class, moved StdDraw into libraries

---
+++ src/LivingEntity.java
@@ -0,0 +1,18 @@
+/*
+Class for the living entities, inheriting Unit.
+A living entity can move and drops a reward when killed.
+*/
+
+public abstract class LivingEntity extends Unit
+{
+    private long speed;
+    private static long reward;
+
+    public LivingEntity(String name, boolean attacker, Element element, long maxHealth, long attack, long range, long speed) 
+    {
+        super(name, attacker, element, maxHealth, attack, range);
+        this.speed = speed;
+    }
+    
+
+}
---
+++ src/Map.java
@@ -0,0 +1,14 @@
+import libraries.StdDraw;
+
+public class Map 
+{
+public static void main(String[] args)
+{
+	Path mapLocation = Paths.get("~.assets/maps/10-10.mtp");
+	StdDraw.setPenRadius(0.05);
+	StdDraw.setPenColor(StdDraw.BLUE);
+	StdDraw.point(0.5, 0.5);
+	StdDraw.setPenColor(StdDraw.MAGENTA);
+	StdDraw.line(0.2, 0.2, 0.8, 0.2); 
+}      
+}
