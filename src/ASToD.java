aad1563360f3b2971b73d54ab4970306ace4e4b4
Apo35310 <apollinairebsy@gmail.com>
2024-12-05 14:47:07 +0100

    Adding the modified main file

--- src/ASToD.java
+++ src/ASToD.java
@@ -1,8 +1,16 @@
+import java.nio.file.Path;
+import java.awt.Color;
+import libraries.StdDraw;
 public class ASToD
 {
 public static void main (String[] args) throws Exception
 {
-	System.out.println ("Hello, World!");
-	Map carte = new Map("5-8");
+	Map carte = new Map("beta_test");
+	StdDraw.setCanvasSize (1024 , 720);
+	StdDraw.setXscale ( -12 , 1012);
+	StdDraw.setYscale ( -10 , 710);
+	StdDraw.setPenColor(Color.BLACK);
+	StdDraw.filledSquare(0.5,0.5,350);
+	StdDraw.enableDoubleBuffering();
 }
 }
