8f265913ab323280b79a404b3d0598d4c65872e1
NyanMaths <nyanmaths@gmail.com>
2025-01-05 21:07:14 +0100

    fixed a compilation error with multiple spawns

--- src/map/MultipleEnemySpawnException.java
+++ src/map/MultipleEnemySpawnException.java
@@ -12,13 +12,28 @@ public class MultipleEnemySpawnException extends Exception
  * Constructor for MultipleEnemySpawnException
  * @param mapName the faulty map
  * @param levelName the calling level identifier
- * @param spawns the list of enemy spawns, excpected to be at list of length 2
+ * @param spawns the list of enemy spawns, excpected to be at least of length 2
  */
 public MultipleEnemySpawnException (String mapName, String levelName, List<Cell> spawns)
 {
-	final String report = "multiple enemy spawns detected :";
-	spawns.stream().forEachOrdered(spawn->report.concat("\nspawn at : "+spawn.getPosition()));
+	super(generateErrorMessage(mapName, levelName, spawns));
+}
 
-	super(report);
+/**
+ * Just generates the error message for the class as Java cries about instructions before super()
+ * @param mapName the faulty map
+ * @param levelName the calling level identifier
+ * @param spawns the list of enemy spawns, excpected to be at list of length 2
+ * @return the message describing the exception
+ */
+private static String generateErrorMessage (String mapName, String levelName, List<Cell> spawns)
+{
+	String report = "multiple enemy spawns detected in " + mapName + "read in level " + levelName + " : ";
+	for (Cell spawn : spawns)
+	{
+		report += "\nspawn at : " + spawn.getPosition();
+	}
+
+	return report;
 }
 }
